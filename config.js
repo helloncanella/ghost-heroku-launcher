89192cb254cfae8933cf26b3165410a0dcfe1897
Hellon Canella <helloncanella@gmail.com>
2016-05-30 23:49:19 +0000

    email

--- config.js
+++ config.js
@@ -42,10 +42,10 @@ config = {
     mail: {
       transport: 'SMTP',
       options: {
-        service: 'Mailgun',
+        service: 'Gmail',
         auth: {
-          user: process.env.MAILGUN_SMTP_LOGIN,
-          pass: process.env.MAILGUN_SMTP_PASSWORD
+          user: 'helloncanella@gmail.com',
+          pass: 'yfbxbujsvfqtlhxz'
         }
       }
     },
